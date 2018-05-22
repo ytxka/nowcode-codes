49d9e7057f55e657b383ea048dbcd7e7d1ba03d4
yanting <819211036@qq.com>
2018-05-22 15:16:56 +0800

    修改了二叉树中和为某一值的路径

--- 二叉树中和为某一值的路径.py
+++ 二叉树中和为某一值的路径.py
@@ -9,6 +9,7 @@ class Solution:
     def FindPath(self, root, expectNumber):
         # write code here
         # 递归实现
+        # 当时突然想到为何函数没有定义完的时候就可以递归调用自己，实际这种矛盾并不存在。
         if not root:
             return []
         elif root and not root.left and not root.right and root.val == expectNumber:
@@ -17,6 +18,7 @@ class Solution:
         left = self.FindPath(root.left, expectNumber - root.val)
         right = self.FindPath(root.right, expectNumber - root.val)
         for i in left + right:
+        	# left、right、i都是list
             res.append([root.val] + i)
         return res
 
