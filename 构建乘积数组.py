9dcf35e6c1b069c5eaef3ae12bde5ec8893a8a7f
yanting <819211036@qq.com>
2018-09-18 15:51:56 +0800

    构建乘积矩阵

--- 构建乘积数组.py
+++ 构建乘积数组.py
@@ -1,16 +1,13 @@
 # -*- coding:utf-8 -*-
 class Solution:
     def multiply(self, A):
-        # 基本方法，时间复杂度太高，不推荐
+        # 分割为上下三角矩阵，详见剑指offer
         if not A:
             return []
-        B = []
-        for i in range(len(A)):
-            tmp = 1
-            for j in range(len(A)):
-                if j == i:
-                    continue
-                else:
-                    tmp *= A[j]
-            B.append(tmp)
-        return B
+        head = [1]
+        tail = [1]
+        for i in range(len(A)-1):
+            head.append(A[i] * head[i])
+            tail.append(A[-1 - i] * tail[i])
+        return [head[j] * tail[-j-1] for j in range(len(head))]
+            
